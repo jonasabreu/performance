c795acbaf548422d0f133e4a420ef12102f029dd
Jonas Abreu <jonas@vidageek.net>
2012-03-24 23:34:53 -0300

    adicionando script para deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+jekyll
+
+cd _site;
+
+typeFor(){
+	TYPE=`echo $1 | sed -E "s/.+\.([^$]+)$/\1/"`
+	CONTENT_TYPE=""
+	if [ $TYPE == "html" ]; then CONTENT_TYPE="text/html"; fi;
+	if [ $TYPE == "txt" ]; then CONTENT_TYPE="text/plain"; fi;
+	if [ $TYPE == "xml" ]; then CONTENT_TYPE="application/xml"; fi;
+	if [ $TYPE == "css" ]; then CONTENT_TYPE="text/css"; fi;
+	if [ $TYPE == "js" ]; then CONTENT_TYPE="text/javascript"; fi;
+}
+
+for X in `find . -type f | sed -E "s/\.\///"`; do 
+	echo "Deploying $X";
+	typeFor $X
+	aws put "x-amz-acl: public-read" "Content-Type: $CONTENT_TYPE" www.javaperformance.com.br $X;
+done;
